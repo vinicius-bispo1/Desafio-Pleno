2e5026f84bbb6c05f08d8fd6de595804034cfa71
vinicius-bispo1 <vini.bispo125@gmail.com>
2025-05-13 13:00:21 -0300

    ✨ feat: add use case to create and save valid transactions

---
+++ src/transactions/use-cases/create-transaction.use-case.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@nestjs/common';
+import { TransactionRepository } from '../repositories/transaction.repository.interface';
+import { Transaction } from '../entities/transaction.entity';
+
+@Injectable()
+export class CreateTransactionUseCase {
+  constructor(private readonly repo: TransactionRepository) {}
+
+  execute(amount: number, timestamp: Date) {
+    const transaction = new Transaction(amount, timestamp);
+    this.repo.add(transaction);
+  }
+}
