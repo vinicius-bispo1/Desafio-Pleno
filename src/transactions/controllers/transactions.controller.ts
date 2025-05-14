8984e08068debe3410f0e428e621e7bed3906751
vinicius-bispo1 <vini.bispo125@gmail.com>
2025-05-14 00:27:07 -0300

    ✨feat: add endpoint to delete all transactions

--- src/transactions/controllers/transactions.controller.ts
+++ src/transactions/controllers/transactions.controller.ts
@@ -1,10 +1,14 @@
-import { Body, Controller, Post, HttpException, HttpStatus } from '@nestjs/common';
+import { Body, Controller, Post, Delete, HttpException, HttpStatus } from '@nestjs/common';
 import { CreateTransactionDto } from '../dto/create-transaction.dto';
 import { CreateTransactionUseCase } from '../use-cases/create-transaction.use-case';
+import { DeleteAllTransactionsUseCase } from '../use-cases/delete-all-transactions.use-case';
 
 @Controller()
 export class TransactionsController {
-  constructor(private readonly createUC: CreateTransactionUseCase) {}
+  constructor(
+    private readonly createUC: CreateTransactionUseCase,
+    private readonly deleteUC: DeleteAllTransactionsUseCase,
+  ) {}
 
   @Post('/transactions')
   create(@Body() dto: CreateTransactionDto) {
@@ -14,5 +18,11 @@ export class TransactionsController {
     } catch (e) {
       throw new HttpException(e.message, HttpStatus.UNPROCESSABLE_ENTITY);
     }
+  }
+
+  @Delete('/transactions')
+  deleteAll() {
+    this.deleteUC.execute();
+    return { message: 'All transactions deleted successfully.' };
   }
 }
