f853b5705e6257de5d172379759ee41fcb4f241e
vinicius-bispo1 <vini.bispo125@gmail.com>
2025-05-14 20:22:31 -0300

    ✨feat: implement getAll to return all transactions

--- src/transactions/interfaces/transaction.repository.interface.ts
+++ src/transactions/interfaces/transaction.repository.interface.ts
@@ -3,4 +3,5 @@ import { Transaction } from '../entities/transaction.entity';
 export interface TransactionRepository {
   add(transaction: Transaction): void;
   deleteAll(): void;
+  getAll(): Transaction[];
 }
