a85c35fea99be19ea73edef792d68399ebae4ddc
vinicius-bispo1 <vini.bispo125@gmail.com>
2025-05-14 01:00:55 -0300

    🧪test: add unit test for DeleteAllTransactionsUseCase

---
+++ src/transactions/use-cases/delete-all-transactions.use-case.spec.ts
@@ -0,0 +1,21 @@
+import { DeleteAllTransactionsUseCase } from './delete-all-transactions.use-case';
+import { TransactionRepository } from '../interfaces/transaction.repository.interface';
+
+describe('DeleteAllTransactionsUseCase', () => {
+  let useCase: DeleteAllTransactionsUseCase;
+  let mockRepo: TransactionRepository;
+
+  beforeEach(() => {
+    mockRepo = {
+      add: jest.fn(),
+      deleteAll: jest.fn(),
+    };
+
+    useCase = new DeleteAllTransactionsUseCase(mockRepo);
+  });
+
+  it('deve chamar o método deleteAll do repositório', () => {
+    useCase.execute();
+    expect(mockRepo.deleteAll).toHaveBeenCalled();
+  });
+});
