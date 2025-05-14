f75719d3a40b5d5154e82a7b2b0fab5381728062
vinicius-bispo1 <vini.bispo125@gmail.com>
2025-05-14 20:41:05 -0300

    🔧chore: register GetStatisticsUseCase in module

--- src/transactions/transactions.module.ts
+++ src/transactions/transactions.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { TransactionsController } from './controllers/transactions.controller';
 import { CreateTransactionUseCase } from './use-cases/create-transaction.use-case';
 import { DeleteAllTransactionsUseCase } from './use-cases/delete-all-transactions.use-case';
+import { GetStatisticsUseCase } from './use-cases/get-statistics.use-case';
 import { InMemoryTransactionRepository } from './repositories/in-memory.transaction.repository';
 
 @Module({
@@ -19,6 +20,11 @@ import { InMemoryTransactionRepository } from './repositories/in-memory.transact
     {
       provide: DeleteAllTransactionsUseCase,
       useFactory: (repo) => new DeleteAllTransactionsUseCase(repo),
+      inject: ['TransactionRepository'],
+    },
+    {
+      provide: GetStatisticsUseCase,
+      useFactory: (repo) => new GetStatisticsUseCase(repo),
       inject: ['TransactionRepository'],
     },
   ],
